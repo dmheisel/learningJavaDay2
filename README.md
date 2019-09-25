53044b29484be8ddf1bc665fb24b440db29eebea
dmheisel <d.m.heisel@gmail.com>
2019-09-25 14:20:55 -0500

    post route completed

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 Check out the database.sql for getting books to work. Books are just an example!
 
 ### GRADLE WRAPPER
-To start your server, type `./gradlew bootrun` 
+To start your server, type `./gradlew bootrun`
 
 Go to: http://localhost:8080/
 
@@ -15,3 +15,17 @@ You should see the hello greeting from the `HelloController` file for GET "/"
 
 To stop, `control + c` (same as node)
 
+# Learning Java Day 2!
+
+## Getting into servers!
+
+Gradle and Springboot are the two things we'll use.
+
+Gradle is the framwork that runs the server
+Springboot is like Express -- allows us to handle the different routes and what to do with them
+
+`./gradlew bootrun` will start the server based on the code in your files, and any time you change server code you need to re-start it.
+Confusingly, it sits at 75% executing forever until you tell it to stop -- this means that your server is running, becauseit hasn't finished.
+
+Springboot provides the controllers.  Things like:
+`@RestController` - which 
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.jdbc.core.JdbcTemplate;
 import hello.Book;
@@ -14,7 +16,7 @@ public class HelloController {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
-    
+
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
@@ -27,7 +29,12 @@ public class HelloController {
             query, new BookRowMapper());
           return books;
     }
-        
-    
-    
-}
+
+    @PostMapping("/book") // PostMapping takes care of Post routes to /book
+    public void addBook(@RequestBody Book newBook) {
+        //creates the "addBook function, which makes a Book object called NewBook off the @RequestBody sent in as JSON data"
+        //access the Book class .getTitle() and getAuthor() to get the newBook's title and author
+        String query = "INSERT into books (title, author) values(\'" + newBook.getTitle() + "\', \'" + newBook.getAuthor() + "\')";
+        jdbcTemplate.update(query); // still need to figure out sanitation!
+    }
+    };
